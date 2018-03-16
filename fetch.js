20b463a54779f302ab9d7852fea1c68b7184463a
ash98 <ashley.burnett@nationalarchives.gsi.gov.uk>
2018-03-16 10:21:32 +0000

    Add let declaration to for each loops

--- fetch.js
+++ fetch.js
@@ -1,15 +1,15 @@
 fetch("/get/filtered-json").then(function(res){
     res.json().then(function (data) {
-        console.log(data);
+
         let h1 = document.getElementById("record_release_count");
         h1.innerHTML = `${data["count"]} records released in the last 24 hours.`;
 
-        for(department in data["departments"]){
+        for(let department in data["departments"]){
             create_li("departments", `${data["departments"][department]["code"]} : ${data["departments"][department]["count"]}`)
         }
 
         if(data["places"].length > 0){
-            for(place in data["places"]){
+            for(let place in data["places"]){
                 create_li("places", `${place} : ${data["places"][place]}`);
             }
         }
@@ -17,11 +17,11 @@ fetch("/get/filtered-json").then(function(res){
             create_li("places", `None of the documents contain semantics on places.`);
         }
 
-        for(taxonomy in data["taxonomies"]){
+        for(let taxonomy in data["taxonomies"]){
             create_li("taxonomies", `${data["taxonomies"][taxonomy]["code"]} : ${data["taxonomies"][taxonomy]["count"]}`);
         }
 
-        for(timePeriod in data["time_periods"]){
+        for(let timePeriod in data["time_periods"]){
             create_li("time_periods", `${data["time_periods"][timePeriod]["code"]} : ${data["time_periods"][timePeriod]["count"]}`);
         }
 
