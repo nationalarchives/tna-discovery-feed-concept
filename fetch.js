7e071394667ef05f1f4cb2eca2a1a31f257a8cd4
ash98 <ashley.burnett@nationalarchives.gsi.gov.uk>
2018-03-16 10:08:50 +0000

    refactor li creation into single function

--- fetch.js
+++ fetch.js
@@ -5,33 +5,28 @@ fetch("/get/filtered-json").then(function(res){
         h1.innerHTML = `${data["count"]} records released in the last 24 hours.`;
 
         for(department in data["departments"]){
-
-            let li = document.createElement("li");
-            li.innerHTML = ` ${data["departments"][department]["code"]} : ${data["departments"][department]["count"]}`;
-            document.getElementById("departments").append(li);
+            create_li("departments", `${data["departments"][department]["code"]} : ${data["departments"][department]["count"]}`)
         }
 
         if(data["places"].length > 0){
             for(place in data["places"]){
-                let li = document.createElement("li");
-                li.innerHTML = ` ${place} : ${data["places"][place]}`;
-                document.getElementById("places").append(li);
+                create_li("places", `${place} : ${data["places"][place]}`);
             }
         }
         else {
-            let li = document.createElement("li");
-            li.innerHTML = `None of the documents contain semantics on places.`;
-            document.getElementById("places").append(li);
+            create_li("places", `None of the documents contain semantics on places.`);
         }
 
-
-
         for(taxonomy in data["taxonomies"]){
-            let li = document.createElement("li");
-            li.innerHTML = ` ${data["taxonomies"][taxonomy]["code"]} : ${data["taxonomies"][taxonomy]["count"]}`;
-            document.getElementById("taxonomies").append(li);
-
+            create_li("taxonomies", `${data["taxonomies"][taxonomy]["code"]} : ${data["taxonomies"][taxonomy]["count"]}`);
         }
 
     })
+
+    function create_li(ul_id, inner_html_data) {
+        let li = document.createElement("li");
+        li.innerHTML = inner_html_data;
+        document.getElementById(ul_id).append(li);
+    }
+
 });
