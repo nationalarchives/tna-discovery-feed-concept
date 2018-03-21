cebc923bbc7c30eb195dea1f784db6f53228c51a
AshTNA <ashleyaburnett@gmail.com>
2018-03-21 15:44:13 +0000

    add taxonomies and clean up handlebar functions

--- functions/handlebar_helpers.js
+++ functions/handlebar_helpers.js
@@ -3,17 +3,20 @@ const fetch = require("node-fetch");
 
 module.exports = {
     departments: function () {
-        console.log(globals.departments_json);
+
+        let departments_data = globals.return_object["departments"];
+        let department_long_names = globals.departments_json["departments"];
+
         let return_data = `<ul>`;
-        for (let i = 0; i < globals.return_object["departments"].length; i++) {
-            console.log();
-            let department_code = globals.departments_json["departments"][globals.return_object["departments"][i]["code"]];
+        for (let i = 0; i < departments_data.length; i++) {
+
+            let department_code = department_long_names[departments_data[i]["code"]];
 
             if(department_code === undefined) {
-                department_code = globals.return_object["departments"][i]["code"];
+                department_code = departments_data[i]["code"];
             }
 
-            let department_count = globals.return_object["departments"][i]["count"];
+            let department_count = departments_data[i]["count"];
 
             return_data = return_data + `<li> ${department_code} : ${department_count} </li>`
         }
@@ -21,16 +24,25 @@ module.exports = {
         return return_data + "</ul>";
     },
     places: function () {
+        let places = globals.return_object["places"];
         let return_data = `<ul>`;
-        if( globals.return_object["places"].length > 0){
+        if( places.length > 0){
 
-            for(let place in globals.return_object["places"]){
-                return_data = return_data + `<li> ${place} : ${data["places"][place]} </li>`;
+            for(let place in places){
+                return_data = return_data + `<li> ${place} : ${places[place]} </li>`;
             }
 
         }
         else {
             return_data = return_data + `<li> None of the documents contain semantics on places. </li>`;
+        }
+        return return_data + "</ul>";
+    },
+    taxonomies: function () {
+        let taxonomies = globals.return_object["taxonomies"];
+        let return_data = `<ul>`;
+        for(let taxonomy in taxonomies){
+           return_data = return_data + `<li> ${taxonomies[taxonomy]["code"]} : ${taxonomies[taxonomy]["count"]} </li>`;
         }
         return return_data + "</ul>";
     }
