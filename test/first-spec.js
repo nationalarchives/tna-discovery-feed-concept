7da80d5074963b552fd77eeca306436ce91ad414
AshTNA <ashleyaburnett@gmail.com>
2018-06-07 10:59:54 +0100

    Change mongo test to use our own mongoose model

--- test/first-spec.js
+++ test/first-spec.js
@@ -28,75 +28,69 @@ describe("testing the discovery API", function () {
 
           done();
         })
-        .catch( function (error){
+        .catch(function (error) {
           done(error);
         });
 
     });
 
-      it('should filter the discovery api JSON', function (done) {
-          let mocked_json = JSON.parse(data.discovery);
-          let filtered_json = tools.filter_JSON_data(mocked_json);
-          assert.ok("records" in filtered_json, "no records in the filtered json");
-          assert.ok("places" in filtered_json, "no places in the filtered json");
-          assert.ok("count" in filtered_json, "no count in the filtered json");
-          assert.ok("taxonomies" in filtered_json, "no taxonomies in the filtered json");
-          assert.ok("time_periods" in filtered_json, "no time_periods in the filtered json");
-          assert.equal(filtered_json["count"], 24);
-          done();
-      });
-
-
-      const mongoose = require('mongoose');
-      const Schema = mongoose.Schema;
-      const user_schema = new Schema({
-      name: {type: String},
-      email: {type: String},
-      username:{type: String},
-      password: {type: String},
-      department_subscriptions: require("../data/empty_department_subscriptions.json"),
-      taxonomy_subscriptions: require("../data/empty_taxonomy_subscriptions.json"),
-      keyword_subscriptions: []
-    })
-    const User = mongoose.model('User', user_schema);
-
-      describe('Database Testing', function () {
-
-        before(function (done) {
-
-          mongoose.connect('mongodb://localhost/notification_feed_test_db');
-
-          const db = mongoose.connection;
-          db.on('error', console.error.bind(console, 'connection error'));
-          db.once('open', function () {
-            console.log('DB connection successful');
-            done();
-          });
-        });
-
-        describe('Test mongo database', function () {
-
-          it('should save a new user', function (done) {
-            var test_user = User({
-              name: 'unit test'
-            });
-
-            test_user.save(done);
-
-          })
-
-        })
-
-
-      });
-
-
-
+    it('should filter the discovery api JSON', function (done) {
+      let mocked_json = JSON.parse(data.discovery);
+      let filtered_json = tools.filter_JSON_data(mocked_json);
+      assert.ok("records" in filtered_json, "no records in the filtered json");
+      assert.ok("places" in filtered_json, "no places in the filtered json");
+      assert.ok("count" in filtered_json, "no count in the filtered json");
+      assert.ok("taxonomies" in filtered_json, "no taxonomies in the filtered json");
+      assert.ok("time_periods" in filtered_json, "no time_periods in the filtered json");
+      assert.equal(filtered_json["count"], 24);
+      done();
+    });
 
 
   })
 
+  const mongoose = require('mongoose');
+  const Schema = mongoose.Schema;
+  const department_subscriptions_data = require("../data/empty_department_subscriptions.json");
+  const taxonomy_subscriptions_data = require("../data/empty_taxonomy_subscriptions.json");
 
+  let User;
+  describe('Database Testing', function () {
+
+    before(function (done) {
+
+      User = require('../models/user');
+
+      const db = mongoose.connection;
+      db.on('error', console.error.bind(console, 'connection error'));
+      db.once('open', function () {
+        console.log('DB connection successful');
+        done();
+      });
+    });
+
+    describe('Test mongo database', function () {
+
+      it('should save a new user', function (done) {
+
+        var newUser = new User({
+          name: 'Ashley',
+          email: 'test@nationalarchives.gov.uk',
+          username: 'ash',
+          password: '123',
+          department_subscriptions: department_subscriptions_data,
+          taxonomy_subscriptions: taxonomy_subscriptions_data,
+          keyword_subscriptions: []
+        });
+
+        User.createUser(newUser, done);
+      })
+
+
+    })
+
+
+  });
 
 });
 
